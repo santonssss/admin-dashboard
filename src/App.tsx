e0b4108968efd083411d2cd1b764ecc24335c8c0
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-10 11:41:21 +0500

    add routes:/documents&chat/calendar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Analytics from "@pages/Analytics";
+import Calendar from "@pages/Calendar";
+import Chat from "@pages/Chat";
+import Documents from "@pages/Documents";
 function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/analytics" element={<Analytics />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/messages" element={<Chat />} />
+          <Route path="/documents" element={<Documents />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
