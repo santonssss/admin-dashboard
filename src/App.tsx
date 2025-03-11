0c1736153027fadcbe27bd4d26c69e3b6975133c
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-12 04:22:45 +0500

    add route to settings page and add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ import Chat from "@pages/Chat";
 import Documents from "@pages/Documents";
 import Users from "@pages/Users";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Settings from "@pages/Settings";
+import { Toaster } from "@components/ui/toaster";
 function App() {
   const queryClient = new QueryClient();
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
+        <Toaster />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -21,6 +24,7 @@ function App() {
             <Route path="/messages" element={<Chat />} />
             <Route path="/documents" element={<Documents />} />
             <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
